57d9cb67524e7aa1934450bd4c73d54d613bb70b
David Huang <dhuang1348@gmail.com>
2014-12-03 22:23:31 -0600

    Change seed generation using MINSTD

--- code/ranlux32.cpp
+++ code/ranlux32.cpp
@@ -24,10 +24,13 @@ namespace rng
          * Initialize seed vector using MINSTD
          */
         static constexpr uint64_t g = 48271;
-        static constexpr uint64_t n = (1UL << 48UL); // 2^49
+        static constexpr uint64_t n = (1UL << 31UL) - 1UL; // 2^32 -1
+        fuint first, second;
         for(size_t i = 1; i < state.size(); ++i)
         {
-            state[i] = static_cast<fuint>((static_cast<uint64_t>(state[i-1]) * g) % n);
+            first = static_cast<fuint>((static_cast<uint64_t>(state[i-1]) * g) % n);
+            second = static_cast<fuint>((static_cast<uint64_t>(first) * g) % n);
+            state[i] = ((static_cast<uint64_t>(first)) << 32UL) << second;
         }
     }
 
